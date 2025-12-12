a17b7ed65b6c19f5768039bd06a1086e66eb6f7f
Hassan Tarek <hassanelashry415@gmail.com>
2025-12-12 13:11:24 +0200

    feat: add notification model and MongoDB repository

---
+++ src/main/java/com/notification/model/Notification.java
@@ -0,0 +1,37 @@
+package com.notification.model;
+
+import com.notification.model.enums.NotificationStatus;
+import com.notification.model.enums.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(value = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String recipient;
+
+    private String message;
+
+    private NotificationType type;
+
+    private NotificationStatus status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private LocalDateTime sentAt;
+}
---
+++ src/main/java/com/notification/model/enums/NotificationStatus.java
@@ -0,0 +1,7 @@
+package com.notification.model.enums;
+
+public enum NotificationStatus {
+    PENDING,
+    SENT,
+    FAILED
+}
